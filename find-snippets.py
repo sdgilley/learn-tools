a91889346cc6b1883daab75c7f4cd4c1e539a766
Sheri Gilley <sgilley@microsoft.com>
2023-11-02 18:24:39 -0500

    add function to read problem files

--- find-snippets.py
+++ find-snippets.py
@@ -5,45 +5,53 @@ See readme for details of the columns in the output file.
 
 ###################### INPUT HERE ############################
 # Name the file and the path to your repo.
-repo_path = 'c:\\GitPrivate\\azure-docs-pr\\articles\\machine-learning\\v1'
-result_fn = "codeowners.txt"
+repo_path = 'c:\\GitPrivate\\azure-docs-pr\\articles\\machine-learning'
+result_fn = "snippets.csv"
 ############################ DONE ############################
 
 # open the file to write the results to.
 f = open(result_fn, 'w+')
-# f.write("from_file, match, branch, path, ref_file, notebook_cell \n")
+f.write("from_file, match, branch, path, ref_file, notebook_cell \n")
 
 # iterate through the files in the folder.  
 import os
 import re
 files = os.listdir(repo_path)
+
+def read_file(file_path):
+    try:
+        with open(file_path, 'r', encoding='utf-8') as target_file:
+            lines = target_file.readlines()
+    except UnicodeDecodeError:
+        try:
+            with open(file_path, 'r', encoding='latin-1') as target_file:
+                lines = target_file.readlines()
+        except Exception as e:
+            print(f"Error reading {file_path}: {e}")
+            lines = []
+    return lines
+
 # find the file names from the directory.  
 for file in files:
     if file.endswith('.md'):
         file_path = os.path.join(repo_path,file)
-        # open the file and read through it line by line.
-        with open(file_path, 'r') as target_file:
-            try: # some of our files contain characters that cause errors.  skip them.
-                for line in target_file.readlines():
-                    # snippets have ~\azureml-examples in them.  Find all snippets and write them to the file.
-                    match_snippet = re.findall(r'\(~\/azureml-examples[^)]*\)|source="~\/azureml-examples[^"]*"', line)
-                    if match_snippet:
-                        for match in match_snippet:
-                            match= match.replace('(', '').replace(')', '').replace('"', '').replace(',', '').replace('source=', '')
-                            # split up the match into parts here.
-                            path = os.path.dirname(match)
-                            ref_file = os.path.basename(match)
-                            branch = path.split('/')[1]
-                            path = path.replace('~', '').replace(branch,'')
-                            if "?" in ref_file:
-                                ref_file, name = ref_file.split('?',1)
-                            else:
-                                name = ''
-
-                            # f.write(f"{file}, {match}, {branch}, {path}, {ref_file}, {name} \n")
-                            f.write(f"{ref_file}, mldocs@microsoft.com \n")
-            except: # record the files that wouldn't open
-                f.write(f"{file}, error\n")
+        lines = read_file(file_path)
+        for line in lines:
+            # snippets have ~\azureml-examples in them.  Find all snippets and write them to the file.
+            match_snippet = re.findall(r'\(~\/azureml-examples[^)]*\)|source="~\/azureml-examples[^"]*"', line)
+            if match_snippet:
+                for match in match_snippet:
+                    match= match.replace('(', '').replace(')', '').replace('"', '').replace(',', '').replace('source=', '')
+                    # split up the match into parts here.
+                    path = os.path.dirname(match)
+                    ref_file = os.path.basename(match)
+                    branch = path.split('/')[1]
+                    path = path.replace('~', '').replace(branch,'')
+                    if "?" in ref_file:
+                        ref_file, name = ref_file.split('?',1)
+                    else:
+                        name = ''
+                f.write(f"{file}, {match}, {branch}, {path}, {ref_file}, {name} \n")    
 # close the csv file.
 f.close()
 
