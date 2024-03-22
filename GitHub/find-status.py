3cb098dcef99dcc55ebd77276261d4bff1ae32c4
Sheri Gilley <sgilley@microsoft.com>
2024-03-22 22:05:03 +0000

    fix find-status

--- GitHub/find-status.py
+++ GitHub/find-status.py
@@ -4,6 +4,9 @@
 # test cases - first one  has no workflow
 # user_input = "~/azureml-examples-main/sdk/python/jobs/single-step/scikit-learn/train-hyperparameter-tune-deploy-with-sklearn/train-hyperparameter-tune-with-sklearn.ipynb?name=credential"
 # user_input = "~/azureml-examples-main/cli/assets/component/train.yml"
+
+import os
+
 if 'user_input' not in globals():
     user_input = input("Enter the file path: ")
 #strip out the query string and ~/azureml-examples- from the input
@@ -13,8 +16,9 @@ branch = user_input.split('/')[0].strip()
 file = user_input.replace(f'{branch}/','').strip()
 wf = file.replace('/','-') # workflow file name uses - for each / in the file path
 wf = wf.replace('sdk-python','sdk') # special case for python files
+wfname, wfext = os.path.splitext(wf)
 
 wf_link = "https://github.com/Azure/azureml-examples/actions/workflows" # where to find the workflows
 gh_link = f"https://github.com/Azure/azureml-examples/blob/{branch}" # where to find the files
-print(f'** Workflow: {wf_link}/{wf}')
+print(f'** Workflow: {wf_link}/{wfname}.yml')
 print(f'** File: {gh_link}/{file}')
