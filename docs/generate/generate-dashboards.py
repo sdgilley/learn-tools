cdbb14bce93813d378429578a9623efef1d802de
Sheri Gilley <sgilley@microsoft.com>
2024-03-28 16:17:37 -0500

    update generate code

--- docs/generate/generate-dashboards.py
+++ docs/generate/generate-dashboards.py
@@ -15,7 +15,11 @@ def get_tutorial_files(file_path):
         file.readline()  # Skip the first line
         for line in file:  # Read the rest of the lines
             line = line.split(',')[0]
-            line = line.replace('https://raw.githubusercontent.com/Azure/azureml-examples/', '/')
+            line = line.replace('https://raw.githubusercontent.com/Azure/azureml-examples/', '')
+            # get rid of the branch name, which is the first part of the path
+            line = '/'.join(line.split('/')[1:])
+            line = '/' + line
+            print(line)
             lines.append(line)
     return lines
 
@@ -27,6 +31,10 @@ def write_html(notebooks):
     today = datetime.now().strftime('%B %d, %Y')
 
     for notebook in notebooks:
+        debug=False
+        if notebook == '/tutorials/get-started-notebooks/quickstart.ipynb':
+            print("Processing quickstart notebook")
+            debug = True
         workflow = notebook.replace('/', '-')[1:]  # Get rid of leading '-'
         workflow = workflow.replace('sdk-python', 'sdk')  # Special case for python files
         workflow = os.path.splitext(workflow)[0] + ".yml"  # Replace extension with .yml
@@ -41,14 +49,17 @@ def write_html(notebooks):
         status = f'<a href="{wf_link}/{workflow}"><img src="{wf_link}/{workflow}/badge.svg?branch=main" alt="{file_name}"></a>'
         row = f'<tr><td>{status}</td><td><a href="{gh_link}/{notebook}">{file}</a></td></tr>\n'
         rows_by_extension[extension].append(row)
+        if debug:
+            print(f'Here is the complete row: {row}')
 
     # Read the top part of the html file from top.html 
     script_dir = os.path.dirname(os.path.realpath(__file__))  # Get the directory that the script is in
-    repo_dir = script_dir
+    repo_dir = script_dir #find the repo directory
     while not os.path.isdir(os.path.join(repo_dir, '.git')):
         repo_dir = os.path.dirname(repo_dir)
     file_dir = os.path.join(repo_dir, "docs")  # Where to put the dashboard files
 
+    # read top and jumps from the script directory
     with open(os.path.join(script_dir, 'top.html'), 'r') as top_file:
         top_contents = top_file.read()
     with open(os.path.join(script_dir, 'jumps.html'), 'r') as jumps_file:
@@ -69,6 +80,11 @@ def write_html(notebooks):
                 file.write('<table>\n')
                 for row in rows:
                     file.write(row)
+                    file.flush()  # Flush the data to the file
+
+                    if 'get-started-notebooks-quickstart' in row:
+                        print(f'HERE IT IS: {row}')  # Print the row
+                        print(f'WRITING IT IN {file_path}')  # Print the file path
                 file.write('</table>\n')
                 file.write('</body>\n</html>')
 # This is the main function
@@ -87,12 +103,12 @@ if __name__ == "__main__":
     tutorial_names = get_tutorial_files(os.path.join(repo_dir, "GitHub","tutorials.csv"))
     # Add the file names from the CODEOWNERS.txt file
     snippet_names = get_snippet_files(os.path.join(repo_dir, "GitHub", "CODEOWNERS.txt"))
-    print(len(snippet_names))
-    file_names = snippet_names + tutorial_names
+
+    file_names = tutorial_names + snippet_names
     print(len(file_names))
     # Replace spaces with '%20' in notebook names
     file_names = [file.replace('\\ ', '%20') for file in file_names]
-
+    # print(file_names)
+    # print(tutorial_names)
     # create the dashboards
-    # write_html(file_names)
-    write_html(tutorial_names)
+    write_html(file_names)
