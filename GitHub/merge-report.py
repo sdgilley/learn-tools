b1f457b31a70d91ff35980feb8688b3de51b73e9
Sheri Gilley <sgilley@microsoft.com>
2024-08-27 11:50:08 -0500

    add comment

--- GitHub/merge-report.py
+++ GitHub/merge-report.py
@@ -23,5 +23,6 @@ for arg in args.input:
 if service == "all":
     m.merge_report(days, "ai")
     m.merge_report(days, "ml")
+    # m.merge_report(days, "fabric") # none used as of now
 else:
     m.merge_report(days, service)
