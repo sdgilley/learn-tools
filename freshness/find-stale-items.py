fc6299a182d746ace4cfa65d8e538b836d7a3505
Sheri Gilley <sgilley@microsoft.com>
2025-03-04 14:21:31 -0600

    more refactoring

--- freshness/find-stale-items.py
+++ freshness/find-stale-items.py
@@ -2,33 +2,36 @@
 # Finds the list of files that need to be refreshed for either this month or next month.
 # creates a csv file with the list of files that need to be refreshed.
 import get_filelist as h
-import fix_items as f
+import fix_titles as f
 import query_work_items as q
 import pandas as pd
 import os
 
 ################################## inputs
-repo_path = "C:/GitPrivate/azure-ai-docs-pr/articles/machine-learning" # your local repo
-offset = 1  # for items going stale next month, use offset = 2. 
+repo_path = "C:/GitPrivate/azure-ai-docs-pr/articles/ai-foundry" # your local repo
+offset = 2  # for items going stale next month, use offset = 2. 
             # for this month's items use offset = 1
-req = 365 # required freshness in days
-csvfile = "mar-ML-work-items.csv" # This is the file that will be created with the work items
-allfiles = "all-files-ML.csv" # This is the file that will be created with all the files
-eng_file = "ml-jan.xlsx" # where the engagement stats are
+req = 90 # required freshness in days
+csvfile = "Apr-foundry-work-items.csv" # This is the file that will be created with the work items
+eng_file = "Jan-engagement.xlsx" # Engagement file to read`
+# eng_file = "ml-jan.xlsx" # Engagement file to read`
+area_path = r"Content\Production\Data and AI\AI Foundry" # area path for querying ADO
+# area_path = r"Content\Production\Data and AI\Machine Learning" # area path for querying ADO
+
 # NOTE: you need to set the Sensitivity label to General on the Excel file
 ################################## end of inputs
 
-# Calculate the cutoff date (90 days before the end of the month)
+# Calculate the cutoff date (N days before the end of the month)
 now = pd.Timestamp.now()
 end_of_month = now + pd.offsets.MonthEnd(offset)
 cutoff_date = end_of_month - pd.Timedelta(days=req)
 print(f"Cutoff date: {cutoff_date}")
-freshness_title = f"Freshness - over {req}:  "  
+freshness_title = f"Freshness - over {req}:"  
+
 
 # get script directory to read/write all files to same directory
 script_dir = os.path.dirname(os.path.realpath(__file__))
 csvfile = os.path.join(script_dir, csvfile)
-allfiles = os.path.join(script_dir, allfiles)
 eng_file = os.path.join(script_dir, eng_file)
 
 # Step 1 - read the engagement stats
@@ -39,9 +42,10 @@ engagement = pd.read_excel(eng_file, sheet_name="Export",
                                             'Flags', 'BounceRate', 'ClickThroughRate', 
                                             'CopyTryScrollRate'])
 # fix the engagement titles to match file metadata:
-engagement = f.fix_engagement(engagement)
+engagement = f.fix_titles(engagement)
 
-# Step 2 - Get dates from the local repo
+# Step 2 - Get dates from the local repo - this is the most recent date, 
+# since engagement is a month old.  Helps to cut out ones already updated.
 # Checkout the branch and pull latest changes if needed...
 # h.checkout(repo_path, "main")
 # get most recent dates from local repo
@@ -59,29 +63,35 @@ articles = articles.merge(engagement, how='right', left_on='title', right_on='Ti
 print(f" After engagement merge, total articles: {articles.shape[0]}")
 # note ms.date is coming from the local repo, not the engagements stats.  
 # engagement stats can be a month old for items just updated last month
-# Convert ms.date to datetime
-articles['ms.date'] = pd.to_datetime(articles['ms.date'], errors='coerce')
+
 
 # Step 3 - find existing work items and merge by title
-work_items = q.query_work_items(freshness_title, req)
-# fix the titles
-work_items = f.fix_work_items(work_items, freshness_title)
+print("Starting query for current work items...")
+work_items = q.query_work_items(freshness_title, area_path, req)
+# fix the titles so that they match the metadata from the repo``
+work_items = f.fix_titles(work_items, freshness_title)
 
 print(f"Total work items: {work_items.shape[0]}")
-# save work items to csv to figure out what happened...
-# work_items.to_csv(os.path.join(script_dir,"work_items.csv"), index=False)
+# DEBUG: save work items to csv to figure out what happened...
+work_items.to_csv(os.path.join(script_dir,"debug-work-items.csv"), index=False)
 
 # now merge to articles
 articles = articles.merge(work_items, how='left', left_on='Title', right_on='Title')
-articles.to_csv(allfiles, index=False)
-print(f" saved all articles to {allfiles}")
+# DEBUG: save all items to csv to figure out what happened...
+articles.to_csv(os.path.join(script_dir,"debug-all-files.csv"), index=False)
+
 # filter out ones with a work item already
 articles = articles[articles['ID'].isnull()]
 print(f"Articles without current work items, before cutoff date: {articles.shape[0]}")
-# create work item spreadsheet for cutoff date
-articles.to_csv(csvfile, index=False)
 
+# Step 4 - filter out articles that are not stale
 cutoff_date = pd.Timestamp(cutoff_date)
+# if ms.date is missing, use LastReviewed instead
+articles['ms.date'] = articles['ms.date'].fillna(articles['LastReviewed'])
+# Convert ms.date to datetime
+articles['ms.date'] = pd.to_datetime(articles['ms.date'], errors='coerce')
+# filter out if beyond the cutoff date
+articles = articles[articles['ms.date'] < cutoff_date]
 print(f"Articles after filtering for cutoff_date of {cutoff_date}: {articles.shape[0]}")
 articles.to_csv(csvfile, index=False)
 print(f"Saved to {csvfile}")
--- freshness/query_work_items.py
+++ freshness/query_work_items.py
@@ -9,7 +9,7 @@ import pandas as pd
 import authenticate_ado as a
 from azure.devops.v7_0.work_item_tracking.models import Wiql
 
-def query_work_items(title_string, days=90):
+def query_work_items(title_string, area_path, days=90):
     project_name = "Content"
     
     # Authenticate with Azure Active Directory (Entra ID)
@@ -22,6 +22,7 @@ def query_work_items(title_string, days=90):
         SELECT [System.Id], [System.Title], [System.State], [System.CreatedDate], [System.IterationPath], [System.AssignedTo]
         FROM workitems
         WHERE [System.TeamProject] = '{project_name}'
+        AND [System.AreaPath] = '{area_path}'
         AND [System.Title] CONTAINS '{title_string}'
         AND [System.State] <> 'Removed'
         """
